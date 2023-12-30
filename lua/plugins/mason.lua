a7eedff6e6d04061f5e46c22a0ed75f1a1437e54
Affan <affantaufiqur@gmail.com>
2023-12-31 01:40:44 +0700

    plugin: mason and bordered cmp

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,22 @@
+return {
+  "williamboman/mason.nvim",
+  opts = {
+    ensure_installed = {
+      "stylua",
+      "shellcheck",
+      "shfmt",
+      "flake8",
+      "ast-grep",
+      "css-lsp",
+      "html-lsp",
+      "json-lsp",
+      "lua-language-server",
+      "markdown-toc",
+      "prettier",
+      "prettierd",
+      "svelte-language-server",
+      "tailwindcss-language-server",
+      "typescript-language-server",
+    },
+  },
+}
