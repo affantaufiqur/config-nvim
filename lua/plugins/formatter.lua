810cdfc0e61f62d8a478aaabfdb26286f34d39ca
Affan <affantaufiqur@gmail.com>
2024-01-03 23:06:51 +0700

    plugin: change keybinding for autocmp

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -3,11 +3,11 @@ return {
   opts = {
     formatters_by_ft = {
       lua = { "stylua" },
-      javascript = { "prettierd", "eslint" },
-      javascriptreact = { "prettierd", "eslint" },
-      typescriptreact = { "prettierd", "eslint" },
-      typescript = { "prettierd", "eslint" },
-      svelte = { "prettierd", "eslint" },
+      javascript = { "prettierd", "prettier", "eslint" },
+      javascriptreact = { "prettierd", "prettier", "eslint" },
+      typescriptreact = { "prettierd", "prettier", "eslint" },
+      typescript = { "prettierd", "prettier", "eslint" },
+      svelte = { "prettierd", "prettier", "eslint" },
     },
   },
 }
