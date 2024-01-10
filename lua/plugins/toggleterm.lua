d61b886a9f6b4345125269f20956a81fad9d7f88
affantaufiqur <affantaufiqur@gmail.com>
2024-01-10 12:50:56 +0700

    remove toggleterm

--- lua/plugins/toggleterm.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "akinsho/toggleterm.nvim",
-  version = "*",
-  config = function()
-    require("toggleterm").setup()
-    vim.keymap.set("n", "<leader>t", ":ToggleTerm<CR>", { noremap = true, silent = true })
-  end,
-}
