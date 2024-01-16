93de14d56e92e2c835b32418c3e749652cc193db
Affan <affantaufiqur@gmail.com>
2024-01-16 20:28:38 +0700

    plugin: treesj

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -9,4 +9,13 @@ return {
       deleteBufferWhenFileDeleted = true,
     },
   },
+  {
+    -- splitting and joing syntax
+    "Wansmer/treesj",
+    keys = { "<space>m", "<space>j", "<space>s" },
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    config = function()
+      require("treesj").setup({})
+    end,
+  },
 }
