f4c9605be086816dd0e64b74d2c4fb39f42441bf
Affan <affantaufiqur@gmail.com>
2024-01-17 01:10:45 +0700

    remove keybind

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -12,13 +12,11 @@ return {
   {
     -- splitting and joing syntax
     "Wansmer/treesj",
-    keys = { "<space>m", "<space>j", "<space>s" },
+    keys = { "<space>m" },
     dependencies = { "nvim-treesitter/nvim-treesitter" },
     config = function()
       require("treesj").setup({})
     end,
   },
-  {
-    "tpope/vim-fugitive",
-  },
+  { "tpope/vim-fugitive" },
 }
