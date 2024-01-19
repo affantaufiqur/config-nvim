f1b60ab5843573ca49aae2675ebb1cf6aad6849d
affantaufiqur <affantaufiqur@gmail.com>
2024-01-19 13:17:43 +0700

    Change diagnostics icons

---
+++ lua/plugins/core.lua
@@ -0,0 +1,14 @@
+return {
+  "LazyVim/LazyVim",
+  opts = {
+    colorscheme = "nord",
+    icons = {
+      diagnostics = {
+        Error = "E ",
+        Warn = "W ",
+        Info = "I ",
+        Hint = "H ",
+      },
+    },
+  },
+}
