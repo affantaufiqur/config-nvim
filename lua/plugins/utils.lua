c17793e26f0bf6a656972ce65c21b1642a2b4e42
affantaufiqur <affantaufiqur@gmail.com>
2024-01-22 16:10:00 +0700

    add dadbod

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -4,4 +4,24 @@ return {
     "tpope/vim-fugitive",
     event = "VeryLazy",
   },
+  {
+    "tpope/vim-dadbod",
+  },
+  {
+    "kristijanhusak/vim-dadbod-ui",
+    dependencies = {
+      { "tpope/vim-dadbod", lazy = true },
+      { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
+    },
+    cmd = {
+      "DBUI",
+      "DBUIToggle",
+      "DBUIAddConnection",
+      "DBUIFindBuffer",
+    },
+    init = function()
+      -- Your DBUI configuration
+      vim.g.db_ui_use_nerd_fonts = 1
+    end,
+  },
 }
