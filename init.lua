2c665a1712af6898e94ea18adb546bbadc325d9d
Affan <affantaufiqur@gmail.com>
2024-05-20 23:55:53 +0700

    added git status and autocmd for refresh

--- init.lua
+++ init.lua
@@ -32,6 +32,26 @@ local function git_branch()
   return ":"
 end
 
+local function git_status()
+  local status = {}
+  local file = io.popen("git status --porcelain 2>nul")
+  if file then
+    local output = file:read("*a")
+    file:close()
+    for line in output:gmatch("[^\r\n]+") do
+      local status_code = line:sub(1, 2)
+      if status_code == "??" then
+        status.untracked = (status.untracked or 0) + 1
+      elseif status_code == " M" or status_code == "M " then
+        status.modified = (status.modified or 0) + 1
+      elseif status_code == " D" or status_code == "D " then
+        status.deleted = (status.deleted or 0) + 1
+      end
+    end
+  end
+  return status
+end
+
 local function statusline()
   local set_color_1 = "%#PmenuSel#"
   local branch = git_branch()
@@ -40,25 +60,44 @@ local function statusline()
   local modified = "%m"
   local align_right = "%="
   local fileencoding = " %{&fileencoding?&fileencoding:&encoding}"
-  local fileformat = " [%{&fileformat}]"
   local filetype = " %y"
   local percentage = " %p%%"
   local linecol = " %l:%c"
+  local git_status_components = {}
+
+  local status = git_status()
+  if status.untracked then
+    table.insert(git_status_components, "[?]" .. status.untracked)
+  end
+  if status.modified then
+    table.insert(git_status_components, "[M]" .. status.modified)
+  end
+  if status.deleted then
+    table.insert(git_status_components, "[D]" .. status.deleted)
+  end
+
+  local git_status_str = table.concat(git_status_components, " ")
 
   return string.format(
     "%s %s %s%s%s%s%s%s%s%s%s",
     set_color_1,
     branch,
     set_color_2,
+    git_status_str,
     file_name,
     modified,
     align_right,
     filetype,
     fileencoding,
-    fileformat,
     percentage,
     linecol
   )
 end
 
 vim.opt.statusline = statusline()
+
+vim.api.nvim_create_user_command("UpdateStatusline", function()
+  vim.opt.statusline = statusline()
+end, {})
+
+vim.api.nvim_set_keymap("n", "<leader>gr", ":UpdateStatusline<CR>", { noremap = true, silent = true })
