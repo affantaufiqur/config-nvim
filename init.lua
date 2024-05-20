bd6a133176ae47cb6fac0ce1ff44dbf639334b23
Affan <affantaufiqur@gmail.com>
2024-05-20 23:32:15 +0700

    use default vim statusline

--- init.lua
+++ init.lua
@@ -19,3 +19,46 @@ if vim.g.neovide then
     change_scale_factor(1 / 1.25)
   end)
 end
+
+local function git_branch()
+  local file = io.popen("git rev-parse --abbrev-ref HEAD 2>nul")
+  if file then
+    local branch = file:read("*a")
+    file:close()
+    if string.len(branch) > 0 then
+      return string.gsub(branch, "[\r\n]+$", "")
+    end
+  end
+  return ":"
+end
+
+local function statusline()
+  local set_color_1 = "%#PmenuSel#"
+  local branch = git_branch()
+  local set_color_2 = "%#LineNr#"
+  local file_name = " %f"
+  local modified = "%m"
+  local align_right = "%="
+  local fileencoding = " %{&fileencoding?&fileencoding:&encoding}"
+  local fileformat = " [%{&fileformat}]"
+  local filetype = " %y"
+  local percentage = " %p%%"
+  local linecol = " %l:%c"
+
+  return string.format(
+    "%s %s %s%s%s%s%s%s%s%s%s",
+    set_color_1,
+    branch,
+    set_color_2,
+    file_name,
+    modified,
+    align_right,
+    filetype,
+    fileencoding,
+    fileformat,
+    percentage,
+    linecol
+  )
+end
+
+vim.opt.statusline = statusline()
--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -16,4 +16,8 @@ return {
     "folke/noice.nvim",
     enabled = false,
   },
+  {
+    "nvim-lualine/lualine.nvim",
+    enabled = false,
+  },
 }
