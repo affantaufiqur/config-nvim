07a9c0518dd89adda50e4a1cf3bc9925c1b26035
affantaufiqur <affantaufiqur@gmail.com>
2024-05-25 10:25:41 +0700

    remove neovide

--- init.lua
+++ init.lua
@@ -1,23 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-if vim.g.neovide then
-  vim.o.guifont = "JetBrainsMono Nerd Font"
-  vim.g.neovide_theme = "auto"
-  vim.g.neovide_cursor_animation_length = 0.0
-  vim.g.neovide_cursor_trail_size = 0
-  vim.g.neovide_scroll_animation_far_lines = 0
-  vim.g.neovide_cursor_animate_command_line = false
-  vim.g.neovide_scale_factor = 0.85
-  local change_scale_factor = function(delta)
-    vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
-  end
-  vim.keymap.set("n", "<C-=>", function()
-    change_scale_factor(1.25)
-  end)
-  vim.keymap.set("n", "<C-->", function()
-    change_scale_factor(1 / 1.25)
-  end)
-end
 
 local function git_branch()
   local os_name = io.popen("uname"):read("*l")
