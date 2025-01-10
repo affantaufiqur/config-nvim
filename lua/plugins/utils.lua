9d9563d76886be818a9b5c25d37f2470c557b1d0
affantaufiqur <affantaufiqur@gmail.com>
2025-01-10 12:57:28 +0700

    remove barbecue

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -91,33 +91,4 @@ return {
       })
     end,
   },
-  {
-    "utilyre/barbecue.nvim",
-    name = "barbecue",
-    version = "*",
-    dependencies = {
-      "SmiteshP/nvim-navic",
-      "nvim-tree/nvim-web-devicons",
-    },
-    opts = {
-      -- configurations go here
-    },
-    config = function()
-      require("barbecue").setup({
-        create_autocmd = false,
-      })
-
-      vim.api.nvim_create_autocmd({
-        "WinScrolled",
-        "BufWinEnter",
-        "CursorHold",
-        "InsertLeave",
-      }, {
-        group = vim.api.nvim_create_augroup("barbecue.updater", {}),
-        callback = function()
-          require("barbecue.ui").update()
-        end,
-      })
-    end,
-  },
 }
