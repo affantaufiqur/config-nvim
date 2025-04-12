ce7dda64f99475cfe553a7e7a011b2be26be1e94
affantaufiqur <affantaufiqur@gmail.com>
2025-04-12 11:39:56 +0700

    set transparent theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,6 +4,9 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
+      require("mana").setup({
+        transparent = true,
+      })
       vim.cmd([[colorscheme mana]])
     end,
   },
