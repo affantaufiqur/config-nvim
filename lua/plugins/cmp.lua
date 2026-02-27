ee3dfe6424767c9c64b017f02ae90d90da80690a
affantaufiqur <affantaufiqur@gmail.com>
2026-02-27 20:56:19 +0700

    adjust blink.cmp fuzzy

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,6 +1,7 @@
 return {
   "saghen/blink.cmp",
   opts = {
+    fuzzy = { implementation = "prefer_rust_with_warning" },
     keymap = {
       preset = "enter",
       ["<S-CR>"] = { "select_and_accept" },
